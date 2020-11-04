2337b0445e90cd3b5e6855576e6fe70274c42b7a
helmitaufiqa <drivehelmi09@gmail.com>
2020-11-04 15:13:48 +0700

    Initial Commit

---
+++ HelmiTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelmiTestProject
+//
+//  Created by helmi taufiq alhakim on 04/11/20.
+//  Copyright © 2020 helmi taufiq alhakim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
